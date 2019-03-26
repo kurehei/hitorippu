237ecdae396e1d362c6809586c6fcd47b43c46ac
ko_kurebayashi <kureykou321@gmail.com>
2019-03-26 14:35:34 +0000

    変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
  # Include RMagick or MiniMagick support:
-  
+  include Cloudinary::CarrierWave
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
