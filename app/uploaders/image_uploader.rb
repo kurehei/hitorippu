b9a199cff3ca08ff05ee57162df9cc09b23c2ceb
ko_kurebayashi <kureykou321@gmail.com>
2019-03-26 13:32:40 +0000

    変化

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
  # Include RMagick or MiniMagick support:
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  else
-    storage :file
-  end
   
-  def public_id
-    model.id
-  end
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
