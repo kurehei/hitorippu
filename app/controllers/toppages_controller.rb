0fc255b82a3857672e9a6aa02ccd8ae36f29a6e0
榑林晃平 <kureykou321@gmail.com>
2020-04-14 02:55:21 +0900

    post_controllerの修正 toppage_controllerの修正 show.html.erbの微修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,12 +1,9 @@
 class ToppagesController < ApplicationController
- 
   def index
-     @posts =Post.all
    if logged_in?
      @posts=current_user.feed_posts.order('created_at DESC').page(params[:page])
-     @post=current_user.posts.find_by(id: params[:id])
      @users = User.order(' created_at DESC')
-     @rank=Post.find(Like.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
+     @rank=Post.joins(:likes).group(:post_id).limit(5).order('count(post_id) desc')
     end
   end
 end
