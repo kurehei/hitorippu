0439087a6980a18ca1b3d99e6045309e262e8db2
榑林晃平 <kureykou321@gmail.com>
2020-04-19 08:21:08 +0900

    各所、viewの調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, -> {order(created_at: :desc)},dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 300 }
