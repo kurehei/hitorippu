c0f60bcd1eec678ff0cbb0a86c706636f10ff2f4
榑林晃平 <kureykou321@gmail.com>
2021-11-12 23:45:31 +0900

    リファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   before_action :set_like, only: [:destroy]
-  
+
   def create
     @like = current_user.likes.build(like_params)
     @post = @like.post
@@ -12,14 +12,14 @@ class LikesController < ApplicationController
 
   def destroy
     @post = @like.post
-    @likes_count = Like.where(post_id: @post.id).count  # ← 追加
+    @likes_count = Like.where(post_id: @like.post.id).count  # ← 追加
     if @like.destroy
       respond_to :js
     end
   end
-  
+
   private
-  
+
   def like_params
     params.permit(:post_id)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,22 @@
 class PostsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
-  
+
   def index
-    @posts=Post.search(params[:name])
+    @posts = Post.search(params[:name])
   end
-  
+
   def new
-    @post= Post.new
+    @post = Post.new
   end
-  
+
  def show
   @post = Post.find(params[:id])
   @likes_count = Like.where(post_id: @post.id).count
   @comments = @post.comments
-  @comment =Comment.new
+  @comment = Comment.new
  end
- 
+
  def create
    @post=current_user.posts.build(post_params)
    if @post.save
@@ -27,24 +27,24 @@ class PostsController < ApplicationController
      render :new
    end
   end
-   
+
   def destroy
     @post.destroy
     flash[:danger]='削除しました。'
     redirect_to root_url
   end
-   
-   private
-  
+
+  private
+
   def correct_user
     @post = current_user.posts.find_by(id: params[:id])
     unless @post
       redirect_to root_url
     end
   end
-  
+
   def post_params
     params.require(:post).permit(:name, :content, :image)
   end
-  
+
 end
